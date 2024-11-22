fb8e908925a6dae57a78011462e56c67a2880043
lauclech <dieumercikoussou95@gmail.com>
2024-11-22 23:05:54 +0100

    premier commit

---
+++ app/connexion/page.jsx
@@ -0,0 +1,13 @@
+import  From  from "@/app/ui/from-connexion"
+import React from "react"
+
+export default function Connexion () {
+    return(
+        <>
+        <div>
+            <h1>Connexion </h1>
+            <From />
+        </div>
+        </>
+    )
+}
---
+++ app/formulaire/page.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+
+export default function Formulaie () {
+    return(
+        <>
+        <div>
+            je sui
+          
+        </div>
+        
+        </>
+    )
+}
